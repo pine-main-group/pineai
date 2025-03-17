5c0c03379f49e347b293b5c54fb73b409f11826f
Taburshunu <ahmadateebarshad@gmail.com>
2025-03-17 20:15:54 +0500

    Delete src directory

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-    return (
-        <main className={"flex justify-center items-center w-scree h-screen"}>
-            <h4 className={"text-5xl font-bold "}>Under <span className={"text-purple-600"}>Development</span></h4>
-        </main>
-    )
-}
-
-export default About
